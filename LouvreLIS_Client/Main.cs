d8506e10a771deb4a3df675f288d0c004496745a
apple246680 <97295135+apple246680@users.noreply.github.com>
2023-08-20 18:17:13 +0800

    Add files via upload

---
+++ LouvreLIS_Client/Main.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LouvreLIS_Client
+{
+    public partial class Main : Form
+    {
+        static Main instance = new Main();
+        public Main()
+        {
+            InitializeComponent();
+            instance=this;
+        }
+        public void so(string name, string mail, string gender, string language,string status, Guid id) {
+            SignInOutBtn.Text = "Sign out";
+            UserNameLabel.Text = "Current User:" + name;
+            AccountInformationBtn_Click(null,null);
+            accountInformationForm.InputData(name,mail,gender,language,status,id);
+            SignInOutBtn.Visible = true;
+            TopPanel.Visible = true;
+        }
+        ModifyForm ModifyForm=new ModifyForm();
+        public static void staticshowmodify() {
+            instance.showmodify();
+        }
+        public void showmodify() 
+        {
+            ShowPanel.Controls.Clear();
+            ModifyForm.TopLevel = false;
+            ShowPanel.Controls.Add(ModifyForm);
+            ModifyForm.GetData(aaa.ID,aaa.EmailAddress,aaa.FirstName,aaa.LastName,aaa.Gender,aaa.Visitor.PreferredLanguage);
+            ModifyForm.Show();
+        }
+        public void showforgotcode(string email,string title)
+        {
+        ForgotcodeForm forgotcodeForm=new ForgotcodeForm();
+            ShowPanel.Controls.Clear();
+            forgotcodeForm.TopLevel = false;
+            ShowPanel.Controls.Add(forgotcodeForm);
+            forgotcodeForm.getdataandrun(email,title);
+            forgotcodeForm.Show();
+        }
+        static public void staticshowforgotcode(string email,string title)
+        {
+            instance.showforgotcode(email,title);
+        }
+        Account aaa = null;
+        static public void staticso(Account account)
+        {
+            instance.so(account.Username,account.EmailAddress,account.Gender,account.Visitor.PreferredLanguage,account.AccountStatus.ToString(),account.ID);
+            instance.aaa = account;    
+        }
+            ForgotForm forgotForm = new ForgotForm();
+        public void showforgot() 
+        {
+            ShowPanel.Controls.Clear();
+            forgotForm.TopLevel = false;
+            ShowPanel.Controls.Add(forgotForm);
+            forgotForm.Show();
+        }
+        static public void staticshowforgot() 
+        { 
+            instance.showforgot();
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            TopPanel.Visible = false;
+            ShowPanel.Controls.Clear();
+            loginForm.TopLevel= false;
+            ShowPanel.Controls.Add(loginForm);
+            loginForm.Show();
+        }
+            AccountInformationForm accountInformationForm = new AccountInformationForm();
+        private void AccountInformationBtn_Click(object sender, EventArgs e)
+        {
+            ShowPanel.Controls.Clear();
+            accountInformationForm.TopLevel = false;
+            ShowPanel.Controls.Add(accountInformationForm);
+            accountInformationForm.Show();
+        }
+        static public void backlogin()
+        {
+            instance.Main_Load(null,null);
+        }
+        private void SignInBtn_Click(object sender, EventArgs e)
+        {
+            if (SignInOutBtn.Text== "Sign In")
+            {
+                Main_Load(null,null);
+                SignInOutBtn.Text = "Sing Out";
+            }
+            else
+            {
+                UserNameLabel.Text = "Current User:";
+                Main_Load(null, null);
+                SignInOutBtn.Text = "Sign In";
+            }
+        }
+    }
+}
