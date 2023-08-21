6a0d6a7d59d86d030ee5bd54859f4807940f52b7
apple246680 <97295135+apple246680@users.noreply.github.com>
2023-08-21 17:21:51 +0800

    Add files via upload

---
+++ LouvreLIS_Management/Login.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace LouvreLIS_Management
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.UsernameTextbox = new System.Windows.Forms.TextBox();
+            this.PasswordTextbox = new System.Windows.Forms.TextBox();
+            this.SigninBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(222, 67);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(80, 17);
+            this.UsernameLabel.TabIndex = 0;
+            this.UsernameLabel.Text = "Username:";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(222, 95);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(78, 17);
+            this.PasswordLabel.TabIndex = 1;
+            this.PasswordLabel.Text = "Password:";
+            // 
+            // UsernameTextbox
+            // 
+            this.UsernameTextbox.Location = new System.Drawing.Point(325, 64);
+            this.UsernameTextbox.Name = "UsernameTextbox";
+            this.UsernameTextbox.Size = new System.Drawing.Size(232, 25);
+            this.UsernameTextbox.TabIndex = 2;
+            // 
+            // PasswordTextbox
+            // 
+            this.PasswordTextbox.Location = new System.Drawing.Point(325, 95);
+            this.PasswordTextbox.Name = "PasswordTextbox";
+            this.PasswordTextbox.Size = new System.Drawing.Size(232, 25);
+            this.PasswordTextbox.TabIndex = 3;
+            // 
+            // SigninBtn
+            // 
+            this.SigninBtn.Location = new System.Drawing.Point(284, 149);
+            this.SigninBtn.Name = "SigninBtn";
+            this.SigninBtn.Size = new System.Drawing.Size(232, 64);
+            this.SigninBtn.TabIndex = 4;
+            this.SigninBtn.Text = "Sign In";
+            this.SigninBtn.UseVisualStyleBackColor = true;
+            this.SigninBtn.Click += new System.EventHandler(this.SigninBtn_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 393);
+            this.Controls.Add(this.SigninBtn);
+            this.Controls.Add(this.PasswordTextbox);
+            this.Controls.Add(this.UsernameTextbox);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Font = new System.Drawing.Font("Arial", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox UsernameTextbox;
+        private System.Windows.Forms.TextBox PasswordTextbox;
+        private System.Windows.Forms.Button SigninBtn;
+    }
+}
---
+++ LouvreLIS_Management/Main.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LouvreLIS_Management
+{
+    public partial class Main : Form
+    {
+        static Main instance = new Main();
+        public Main()
+        {
+            InitializeComponent();
+            instance=this;
+        }
+        Account account = null;
+        public void getlogindata(Account asd)
+        {
+            SignBtn.Text = "Sign Out";
+            UsernameLabel.Text = "Current User:" + asd.Username;
+            account = asd;
+            AccountInfoBtn_Click(null, null);
+            AccountInfoBtn.Visible = true;
+        }
+        static public void staticgetlogindata(Account asd)
+        {
+            instance.getlogindata(asd);
+        }
+        private void SignBtn_Click(object sender, EventArgs e)
+        {
+            if (SignBtn.Text=="Sign In")
+            {
+                LoginForm loginForm=new LoginForm();
+                ShowPanel.Controls.Clear();
+                loginForm.TopLevel = false;
+                ShowPanel.Controls.Add(loginForm);
+                loginForm.Show();
+            }
+            else
+            {
+                UsernameLabel.Text = "Current User:Not Signed In";
+                SignBtn.Text = "Sign In";
+                SignBtn_Click(sender,e);
+                AccountInfoBtn.Visible = false;
+            }
+        }
+        AccountInfoForm accountInfoForm=new AccountInfoForm();
+        private void AccountInfoBtn_Click(object sender, EventArgs e)
+        {
+            ShowPanel.Controls.Clear();
+            accountInfoForm.TopLevel = false;
+            ShowPanel.Controls.Add(accountInfoForm);
+            accountInfoForm.run(account);
+            accountInfoForm.Show();
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            SignBtn_Click(sender, e);
+        }
+    }
+}
