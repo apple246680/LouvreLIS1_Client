f5a5a16c5f549aa830656fb199833611ab8d9981
apple246680 <pengy4875@gmail.com>
2023-08-28 22:49:04 +0800

    今天晚上剛開始做

---
+++ WSC3/WSC3/Global.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSC2
+{
+    public static class Global
+    {
+        public static void ShowSearchProperties()
+        {
+            Main.ins.Controls.Clear();
+            SearchPropertiesOrListingsForm searchProperties = new SearchPropertiesOrListingsForm();
+            Main.ins.Controls.Add(searchProperties);
+            searchProperties.Show();
+        }
+    }
+}
